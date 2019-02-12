2e934d8ec7fc83bdbceaebbff6587722b7cac72c
nsbtr <ngsabater@gmail.com>
2019-02-12 02:32:05 -0500

    Remove unnecessary exports

--- src/actions.js
+++ src/actions.js
@@ -4,21 +4,21 @@ export const UPDATE_FORKED_EVENTS = 'UPDATE_FORKED_EVENTS';
 export const UPDATE_PULL_REQUEST_EVENTS = 'UPDATE_PULL_REQUEST_EVENT';
 export const SET_LOGGED_IN = 'SET_LOGGED_IN';
 
-export const updateForkedEvents = payload => ({
+const updateForkedEvents = payload => ({
   type: UPDATE_FORKED_EVENTS,
   payload
 });
-export const updatePullRequestEvents = payload => ({
+const updatePullRequestEvents = payload => ({
   type: UPDATE_PULL_REQUEST_EVENTS,
   payload
 });
 
-export const setLoggedIn = payload => ({
+const setLoggedIn = payload => ({
   type: SET_LOGGED_IN,
   payload
 });
 
-export const setHasError = payload => ({
+const setHasError = payload => ({
   type: SET_LOGGED_IN,
   payload
 });
